11111e4b9215b4aa177d154db5246b5638001957
raghavajag <raghavajagarwal@gmail.com>
2024-12-26 19:39:28 +0530

    feat: running election timer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module raft
 
 go 1.22.2
+
+require golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
--- raft.go
+++ raft.go
@@ -1,55 +1,106 @@
 package main
 
-import "sync"
+import (
+	"log"
+	"sync"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
 
 type CMState int
 
 const (
-    Follower CMState = iota
-    Candidate
-    Leader
-    Dead
+	Follower CMState = iota
+	Candidate
+	Leader
+	Dead
 )
 
 func (s CMState) String() string {
-    switch s {
-    case Follower:
-        return "Follower"
-    case Candidate:
-        return "Candidate"
-    case Leader:
-        return "Leader"
-    case Dead:
-        return "Dead"
-    default:
-        panic("unreachable")
-    }
+	switch s {
+	case Follower:
+		return "Follower"
+	case Candidate:
+		return "Candidate"
+	case Leader:
+		return "Leader"
+	case Dead:
+		return "Dead"
+	default:
+		panic("unreachable")
+	}
 }
 
 // ConsensusModule represents the core Raft consensus module
 type ConsensusModule struct {
-    mu sync.Mutex
+	mu sync.Mutex
 
-    id      int
-    peerIds []int
-    server  *Server
+	id      int
+	peerIds []int
+	server  *Server
 
-    // Persistent Raft state
-    currentTerm int
-    votedFor    int
+	// Persistent Raft state
+	currentTerm int
+	votedFor    int
 
-    // Volatile Raft state
-    state CMState
+	// Volatile Raft state
+	state CMState
+
+	electionResetEvent time.Time
 }
 
 func NewConsensusModule(id int, peerIds []int, server *Server) *ConsensusModule {
-    cm := &ConsensusModule{
-        id:          id,
-        peerIds:     peerIds,
-        server:      server,
-        state:       Follower,
-        votedFor:    -1,
-        currentTerm: 0,
-    }
-    return cm
+	cm := &ConsensusModule{
+		id:                 id,
+		peerIds:            peerIds,
+		server:             server,
+		state:              Follower,
+		votedFor:           -1,
+		currentTerm:        0,
+		electionResetEvent: time.Now(),
+	}
+	go cm.runElectionTimer()
+	return cm
+}
+
+func (cm *ConsensusModule) electionTimeout() time.Duration {
+	// Election timeout is randomly chosen between 150-300ms
+	return time.Duration(150+rand.Intn(150)) * time.Millisecond
+}
+
+func (cm *ConsensusModule) runElectionTimer() {
+	timeoutDuration := cm.electionTimeout()
+	cm.mu.Lock()
+	termStarted := cm.currentTerm
+	cm.mu.Unlock()
+
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+
+		cm.mu.Lock()
+		if cm.state != Candidate && cm.state != Follower {
+			log.Printf("Stopping election timer for server %d (not Candidate or Follower)\n", cm.id)
+			cm.mu.Unlock()
+			return
+		}
+
+		if termStarted != cm.currentTerm {
+			log.Printf("Stopping election timer for server %d (term changed)\n", cm.id)
+			cm.mu.Unlock()
+			return
+		}
+
+		// Start election if we haven't heard from leader in timeoutDuration
+		if elapsed := time.Since(cm.electionResetEvent); elapsed >= timeoutDuration {
+			log.Printf("Starting election for server %d\n", cm.id)
+			// cm.startElection()
+			cm.mu.Unlock()
+			return
+		}
+		cm.mu.Unlock()
+	}
 }
